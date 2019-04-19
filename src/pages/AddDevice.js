65f320b9b8a63262e356b306158052726dc7eb26
Artur Górowski <artek159@gmail.com>
2019-04-19 14:59:46 +0200

    devices

--- src/pages/AddDevice.js
+++ src/pages/AddDevice.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react'
 import {
-    View, 
-    Text, 
+    View,
+    Text,
     StyleSheet,
-    ListView
+    ListView,
+    ScrollView
 } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
-export default class AddDevice extends Component{
+export default class AddDevice extends Component {
     constructor(props) {
         super(props);
-        
 
         this.state = {
             dataSource: ds
-        };   
+        };
     }
     componentDidMount = async () => {
-        
+
         try {
             const response = await fetch('https://intelligent-home.herokuapp.com/api/devices', {
                 method: 'GET'
@@ -44,26 +45,32 @@ export default class AddDevice extends Component{
       }*/
 
 
-      render() {
-        return (
-            <ListView
-                style={styles.container}
-                dataSource={this.state.dataSource}
-                renderRow={(rowData) =>
-                    <View>
-                        <Text>id: {rowData.id_device}</Text>
-                        <Text>name: {rowData.name}</Text>
-                    </View>
-                   }
-            />
-        );
-    }
+    render() {
+      return (
+          <ListView
+              style={styles.container}
+              dataSource={this.state.dataSource}
+              renderRow={(rowData) =>
+                  <View>
+                    <TouchableOpacity style={styles.device}>
+                        <Text>{rowData.name}</Text>
+                    </TouchableOpacity> 
+                  </View>
+                 }
+          />
+      );
+  }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        
         //backgroundColor: '#99CC33',
     },
+    device:{
+        width: 300, 
+        height: 50,
+        margin: 15,
+        justifyContent: 'center'
+    }
 });
